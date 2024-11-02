1b48a9943c3fd53c678770ace17325ae4c4bd8ff
Vincent Amberger <ambergervincent2000@gmail.com>
2024-11-02 15:19:37 +0100

    first commit - Led Blinker HAL

---
+++ LED Blinker/driver_init.c
@@ -0,0 +1,70 @@
+/*
+ * Code generated from Atmel Start.
+ *
+ * This file will be overwritten when reconfiguring your Atmel Start project.
+ * Please copy examples or other code you want to keep to a separate file
+ * to avoid losing it when reconfiguring.
+ */
+
+#include "driver_init.h"
+#include <peripheral_clk_config.h>
+#include <utils.h>
+#include <hal_init.h>
+
+struct timer_descriptor TIMER_0;
+
+void delay_driver_init(void)
+{
+	delay_init(SysTick);
+}
+
+/**
+ * \brief Timer initialization function
+ *
+ * Enables Timer peripheral, clocks and initializes Timer driver
+ */
+static void TIMER_0_init(void)
+{
+	hri_mclk_set_APBAMASK_TC1_bit(MCLK);
+	hri_gclk_write_PCHCTRL_reg(GCLK, TC1_GCLK_ID, CONF_GCLK_TC1_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+
+	timer_init(&TIMER_0, TC1, _tc_get_timer());
+}
+
+void system_init(void)
+{
+	init_mcu();
+
+	// GPIO on PB31
+
+	// Set pin direction to input
+	gpio_set_pin_direction(SW0, GPIO_DIRECTION_IN);
+
+	gpio_set_pin_pull_mode(SW0,
+	                       // <y> Pull configuration
+	                       // <id> pad_pull_config
+	                       // <GPIO_PULL_OFF"> Off
+	                       // <GPIO_PULL_UP"> Pull-up
+	                       // <GPIO_PULL_DOWN"> Pull-down
+	                       GPIO_PULL_UP);
+
+	gpio_set_pin_function(SW0, GPIO_PIN_FUNCTION_OFF);
+
+	// GPIO on PC18
+
+	gpio_set_pin_level(LED_0,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   true);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(LED_0, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(LED_0, GPIO_PIN_FUNCTION_OFF);
+
+	delay_driver_init();
+
+	TIMER_0_init();
+}
---
+++ LED Blinker/main.c
@@ -0,0 +1,57 @@
+#include <atmel_start.h>
+
+#define TIMER_INTERVAL_SLOW 500
+#define TIMER_INTERVAL_FAST 100
+
+static struct timer_task TIMER_0_task1;
+
+static void TIMER_0_task1_cb(const struct timer_task *const timer_task)
+{
+	gpio_toggle_pin_level(LED_0);
+}
+
+void configure_timer(uint16_t interval);
+volatile bool button_pressed = false;
+
+int main(void)
+{
+	/* Initializes MCU, drivers and middleware */
+	atmel_start_init();
+	
+	configure_timer(TIMER_INTERVAL_SLOW);
+	
+	/* Replace with your application code */
+	while (1) {
+		
+		if(gpio_get_pin_level(SW0) == false){
+			if(!button_pressed){
+				button_pressed = true;
+				timer_stop(&TIMER_0);
+				timer_remove_task(&TIMER_0, &TIMER_0_task1);
+				configure_timer(TIMER_INTERVAL_FAST);
+			}
+		}else{
+			if(button_pressed){
+				button_pressed = false;
+				timer_stop(&TIMER_0);
+				timer_remove_task(&TIMER_0, &TIMER_0_task1);
+				configure_timer(TIMER_INTERVAL_SLOW);
+			}
+		}
+		
+	}
+}
+
+
+// Initialisierung und Konfiguration des Timers
+void configure_timer( uint16_t interval ) {
+
+	TIMER_0_task1.interval = interval;
+	TIMER_0_task1.cb = TIMER_0_task1_cb;
+	TIMER_0_task1.mode = TIMER_TASK_REPEAT;
+
+	// Timer starten
+	timer_add_task(&TIMER_0, &TIMER_0_task1);  // Nutze TIMER_0
+	//timer_stop(&TIMER_0);  // Standardm‰ﬂig Timer stoppen
+	timer_start(&TIMER_0);
+}
